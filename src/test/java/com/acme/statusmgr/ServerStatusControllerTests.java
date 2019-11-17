5528250786535ac4c761f0673950cb99cc3f3137
grabinow2 <grabinow2@student.touro.edu>
2019-11-16 19:18:43 -0500

    It seems this file was left out of the last commit; this was intended to be part of the package restructuring.

--- src/test/java/com/acme/statusmgr/ServerStatusControllerTests.java
+++ src/test/java/com/acme/statusmgr/ServerStatusControllerTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package statusmgr;
+package com.acme.statusmgr;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,7 +27,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
