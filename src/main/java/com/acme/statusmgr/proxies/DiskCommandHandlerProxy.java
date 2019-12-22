1621d945b1dfbfb3e9c13964451778c8f833116a
grabinow2 <grabinow2@student.touro.edu>
2019-12-22 17:20:12 -0500

    story 3.

--- src/main/java/com/acme/statusmgr/proxies/DiskCommandHandlerProxy.java
+++ src/main/java/com/acme/statusmgr/proxies/DiskCommandHandlerProxy.java
@@ -1,16 +1,33 @@
 package com.acme.statusmgr.proxies;
 
 import com.acme.statusmgr.commands.DiskStatusCommand;
+import com.acme.statusmgr.executors.SerialExecutor;
 
 public class DiskCommandHandlerProxy {
 
     private DiskStatusCommand cmd;
+
+    private static boolean isThreadRunning;
 
     public DiskCommandHandlerProxy(DiskStatusCommand cmd) {
         this.cmd = cmd;
     }
 
     public void runMultiThreaded() {
-        //todo logic for checking that only one other thread be run at one time
+
+        if (isThreadRunning){
+            return;
+        }
+        else {
+            isThreadRunning = true;
+
+            Thread thread = new Thread(() -> {
+                SerialExecutor serialExecutor = new SerialExecutor(cmd);
+                serialExecutor.handleImmediately();
+            });
+
+        }
+
     }
+
 }
